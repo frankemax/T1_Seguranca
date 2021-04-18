4ed7d6049929fc5d35a7196ae827ae6bd1189e87
Max Franke <max.franke@acad.pucrs.br>
2021-04-18 02:01:35 -0300

    init

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+    }
+
+    public static void readFile() {
+    }
+
+    public static void searchKeySize() {
+        double coincidenceChain = 0.065;
+        double randomChain = 0.038;
+    }
+
+}
