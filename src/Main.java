564ea601b2112c829d634ff00303825fc8d9f18b
Max Franke <max.franke@acad.pucrs.br>
2021-04-19 03:31:12 -0300

    naofunciona com DemCifrado

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,28 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 //Max Franke
 
 public class Main {
     private static String rawText;
-    private static String decryptText;
     private static int keySize;
     private static String key;
+    private static String decryptText;
 
-    private static double coincidenceChain = 0.065;
-    private static char freqLetter = 'e';
-
+    private static double coincidenceChain;
+    private static char freqLetter;
+    private static List<Character> freqChain;
+    private static int iter = 0;
 
     public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+
         System.out.println("""
                     ___                           _            \s
                    /   \\___  ___ _ __ _   _ _ __ | |_ ___  _ __\s
@@ -25,41 +33,73 @@ public class Main {
                                       |___/|_|                  \s""");
 
         System.out.println("==========================================================================");
+        System.out.println("O texto esta em qual linguagem? (1 = Portugues / 2 = Ingles) ");
+        setLanguageConfigs(scanner.nextInt());
+
+
         System.out.println("Reading encrypted file...");
         readFile();
         System.out.println("File read ! File size: " + rawText.length());
         System.out.println("==========================================================================");
 
-        System.out.println("Discovering key size...");
-        searchKeySize();
-        System.out.println("Key Size found! : " + keySize);
-        System.out.println("==========================================================================");
+        questioning:
+        while (true) {
+            freqLetter = freqChain.get(iter);
+            System.out.println("Discovering key size...");
+            searchKeySize();
+            System.out.println("Key Size found! : " + keySize);
+            System.out.println("==========================================================================");
 
-        System.out.println("Discovering key...");
-        discoverKey(keySize);
-        System.out.println("Key found! : " + key);
-        System.out.println("==========================================================================");
+            System.out.println("Discovering key...");
+            discoverKey(keySize);
+            System.out.println("Key found! : " + key);
+            System.out.println("==========================================================================");
 
-        System.out.println("Starting decryption... ");
-        decryptMessage(key, keySize);
-        System.out.println("Message decrypted sucessfully!");
-        System.out.println("==========================================================================");
+            System.out.println("Starting decryption... ");
+            decryptMessage(key, keySize);
+            System.out.println("Message decrypted successfully!");
+            System.out.println("==========================================================================");
 
-        System.out.println("Generating report...");
-        writeFile(decryptText);
-        System.out.println("Report generated sucessfully !! See output.txt");
+            System.out.println("Seems right?: (1 = Y / 2 = N) ");
+            System.out.println(decryptText.substring(0, 50));
+            numero = scanner.nextInt();
+
+            if (numero == 1) {
+                System.out.println("Generating report...");
+                writeFile(decryptText);
+                System.out.println("Report generated successfully !! See output.txt");
+                break questioning;
+            }
+
+            System.out.println("==========================================================================");
+            System.out.println("Trying again...");
+
+            iter++;
+        }
+
 
     }
 
+    public static void setLanguageConfigs(int number) {
+        if (number == 1) {
+            freqChain = new ArrayList(Arrays.asList('a', 'e', 'o', 's', 'i'));
+            coincidenceChain = 0.072723;
+        }
+
+        if (number == 2) {
+            freqChain = new ArrayList(Arrays.asList('e', 't', 'a', 'o', 'i'));
+            coincidenceChain = 0.065;
+        }
+    }
 
     public static void readFile() throws IOException {
-        try (BufferedReader br = new BufferedReader(new FileReader("cypherTexts/cipher1.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("cypherTexts/DemCifrado.txt"))) {
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
 
             while (line != null) {
                 sb.append(line);
-                sb.append(System.lineSeparator());
+                //sb.append(System.lineSeparator());
                 line = br.readLine();
             }
             rawText = sb.toString();
@@ -79,7 +119,7 @@ public class Main {
     }
 
     public static void searchKeySize() {
-        for (int i = 1; i < 5000; i++) {
+        for (int i = 1; i < 20; i++) {
             if (coincidenceIndex(i)) {
                 //System.out.println("keySize :" + i);
                 keySize = i;
@@ -90,7 +130,7 @@ public class Main {
 
     private static void decryptMessage(String key, int keySize) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < rawText.length() - 2; i++) {
+        for (int i = 0; i < rawText.length(); i++) {
             if ((rawText.charAt(i) - key.charAt(i % keySize) + 97) < 97) {
                 sb.append((char) (rawText.charAt(i) - key.charAt(i % keySize) + 97 + 26));
             } else {
@@ -109,7 +149,7 @@ public class Main {
             double sum = 0.0;
             int[] alphabet = new int[26];
 
-            for (int j = i; j < rawText.length() - 2; j += keySize) {
+            for (int j = i; j < rawText.length(); j += keySize) {
                 alphabet[rawText.charAt(j) - 97]++;
                 n++;
             }
@@ -120,11 +160,13 @@ public class Main {
 
             double ic = sum / (n * (n - 1));
 
+            //System.out.println(total);
             total += ic;
         }
 
         total = total / keySize;
-        return total < coincidenceChain + 0.003 && total > coincidenceChain - 0.003;
+        //System.out.println(keySize + " " + total);
+        return total < coincidenceChain + 0.005 && total > coincidenceChain - 0.005;
 
     }
 
@@ -132,7 +174,7 @@ public class Main {
         StringBuilder keyBuilder = new StringBuilder();
         for (int i = 0; i < keySize; i++) {
             StringBuilder sb = new StringBuilder();
-            for (int j = i; j < rawText.length() - 2; j += keySize) {
+            for (int j = i; j < rawText.length(); j += keySize) {
                 sb.append(rawText.charAt(j));
             }
 
