8b67f3aab4c5f221f72b859c53fbd7fec79e17b2
Max Franke <max.franke@acad.pucrs.br>
2021-04-19 01:25:46 -0300

    clean

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+//Max Franke
+
 public class Main {
     private static String rawText;
     private static String decryptText;
+    private static int keySize;
     private static String key;
 
     private static double coincidenceChain = 0.065;
@@ -13,16 +16,44 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
+        System.out.println("""
+                    ___                           _            \s
+                   /   \\___  ___ _ __ _   _ _ __ | |_ ___  _ __\s
+                  / /\\ / _ \\/ __| '__| | | | '_ \\| __/ _ \\| '__|
+                 / /_//  __/ (__| |  | |_| | |_) | || (_) | |  \s
+                /___,' \\___|\\___|_|   \\__, | .__/ \\__\\___/|_|  \s
+                                      |___/|_|                  \s""");
+
+        System.out.println("==========================================================================");
+        System.out.println("Reading encrypted file...");
         readFile();
-        int keySize = searchKeySize();
-        key = discoverKey(keySize);
+        System.out.println("File read ! File size: " + rawText.length());
+        System.out.println("==========================================================================");
+
+        System.out.println("Discovering key size...");
+        searchKeySize();
+        System.out.println("Key Size found! : " + keySize);
+        System.out.println("==========================================================================");
+
+        System.out.println("Discovering key...");
+        discoverKey(keySize);
+        System.out.println("Key found! : " + key);
+        System.out.println("==========================================================================");
+
+        System.out.println("Starting decryption... ");
         decryptMessage(key, keySize);
+        System.out.println("Message decrypted sucessfully!");
+        System.out.println("==========================================================================");
+
+        System.out.println("Generating report...");
         writeFile(decryptText);
+        System.out.println("Report generated sucessfully !! See output.txt");
 
     }
 
+
     public static void readFile() throws IOException {
-        try (BufferedReader br = new BufferedReader(new FileReader("cypherTexts/20201-teste1.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("cypherTexts/cipher1.txt"))) {
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
 
@@ -40,21 +71,21 @@ public class Main {
             FileWriter myWriter = new FileWriter("output.txt");
             myWriter.write(s);
             myWriter.close();
-            System.out.println("Successfully wrote to the file.");
+            //System.out.println("Successfully wrote to the file.");
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
     }
 
-    public static int searchKeySize() {
-        for (int i = 1; i < 20; i++) {
+    public static void searchKeySize() {
+        for (int i = 1; i < 5000; i++) {
             if (coincidenceIndex(i)) {
-                System.out.println("keySize :" + i);
-                return i;
+                //System.out.println("keySize :" + i);
+                keySize = i;
+                break;
             }
         }
-        return 0;
     }
 
     private static void decryptMessage(String key, int keySize) {
@@ -66,7 +97,7 @@ public class Main {
                 sb.append((char) (rawText.charAt(i) - key.charAt(i % keySize) + 97));
             }
         }
-        System.out.println("message: " + sb.toString());
+        //System.out.println("message: " + sb.toString());
         decryptText = sb.toString();
 
     }
@@ -97,7 +128,7 @@ public class Main {
 
     }
 
-    public static String discoverKey(int keySize) {
+    public static void discoverKey(int keySize) {
         StringBuilder keyBuilder = new StringBuilder();
         for (int i = 0; i < keySize; i++) {
             StringBuilder sb = new StringBuilder();
@@ -110,10 +141,10 @@ public class Main {
         }
 
 
-        System.out.println("key: " + keyBuilder.toString());
+        //System.out.println("key: " + keyBuilder.toString());
 
 
-        return keyBuilder.toString();
+        key = keyBuilder.toString();
     }
 
     public static char getFreqLetter(String s) {
